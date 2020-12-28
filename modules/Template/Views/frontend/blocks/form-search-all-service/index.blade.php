d270149b2d1e6fec559f5c44056b12b0a9ff84ae
LunA721 <rh7214you@gmail.com>
2020-12-29 01:15:17 +0900

    change form search style

--- modules/Template/Views/frontend/blocks/form-search-all-service/index.blade.php
+++ modules/Template/Views/frontend/blocks/form-search-all-service/index.blade.php
@@ -1,50 +1,50 @@
-
-<div class="bravo-form-search-all" style="background-image: linear-gradient(0deg,rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)),url('{{$bg_image_url}}') !important">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="text-heading">{{$title}}</h1>
-                <div class="sub-heading">{{$sub_title}}</div>
-                @if(empty($hide_form_search))
-                    <div class="g-form-control">
-                        <ul class="nav nav-tabs" role="tablist">
-                            @if(!empty($service_types))
-                                @php $number = 0; @endphp
-                                @foreach ($service_types as $service_type)
-                                    @php
-                                        $allServices = get_bookable_services();
-                                        if(empty($allServices[$service_type])) continue;
-                                        $module = new $allServices[$service_type];
-                                    @endphp
-                                    <li role="bravo_{{$service_type}}">
-                                        <a href="#bravo_{{$service_type}}" class="@if($number == 0) active @endif" aria-controls="bravo_{{$service_type}}" role="tab" data-toggle="tab">
-                                            <i class="{{ $module->getServiceIconFeatured() }}"></i>
-                                            {{ !empty($modelBlock["title_for_".$service_type]) ? $modelBlock["title_for_".$service_type] : $module->getModelName() }}
-                                        </a>
-                                    </li>
-                                    @php $number++; @endphp
-                                @endforeach
-                            @endif
-                        </ul>
-                        <div class="tab-content">
-                            @if(!empty($service_types))
-                                @php $number = 0; @endphp
-                                @foreach ($service_types as $service_type)
-                                    @php
-                                        $allServices = get_bookable_services();
-                                        if(empty($allServices[$service_type])) continue;
-                                        $module = new $allServices[$service_type];
-                                    @endphp
-                                    <div role="tabpanel" class="tab-pane @if($number == 0) active @endif" id="bravo_{{$service_type}}">
-                                        @include(ucfirst($service_type).'::frontend.layouts.search.form-search')
-                                    </div>
-                                    @php $number++; @endphp
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                @endif
-            </div>
-        </div>
-    </div>
-</div>
+
+<div class="bravo-form-search-all" style="background-image: linear-gradient(0deg,rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)),url('{{$bg_image_url}}') !important;height:500px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="text-heading">{{$title}}</h1>
+                <div class="sub-heading">{{$sub_title}}</div>
+                @if(empty($hide_form_search))
+                    <div class="g-form-control">
+                        <ul class="nav nav-tabs" role="tablist">
+                            @if(!empty($service_types))
+                                @php $number = 0; @endphp
+                                @foreach ($service_types as $service_type)
+                                    @php
+                                        $allServices = get_bookable_services();
+                                        if(empty($allServices[$service_type])) continue;
+                                        $module = new $allServices[$service_type];
+                                    @endphp
+                                    <li role="bravo_{{$service_type}}">
+                                        <a href="#bravo_{{$service_type}}" class="@if($number == 0) active @endif" aria-controls="bravo_{{$service_type}}" role="tab" data-toggle="tab">
+                                            <i class="{{ $module->getServiceIconFeatured() }}"></i>
+                                            {{ !empty($modelBlock["title_for_".$service_type]) ? $modelBlock["title_for_".$service_type] : $module->getModelName() }}
+                                        </a>
+                                    </li>
+                                    @php $number++; @endphp
+                                @endforeach
+                            @endif
+                        </ul>
+                        <div class="tab-content">
+                            @if(!empty($service_types))
+                                @php $number = 0; @endphp
+                                @foreach ($service_types as $service_type)
+                                    @php
+                                        $allServices = get_bookable_services();
+                                        if(empty($allServices[$service_type])) continue;
+                                        $module = new $allServices[$service_type];
+                                    @endphp
+                                    <div role="tabpanel" class="tab-pane @if($number == 0) active @endif" id="bravo_{{$service_type}}">
+                                        @include(ucfirst($service_type).'::frontend.layouts.search.form-search')
+                                    </div>
+                                    @php $number++; @endphp
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
---
+++ storage/framework/views/fc0b9dd4b05040a9c7aad4497f835a91d9a835a8.php
@@ -0,0 +1,34 @@
+<div class="bravo-list-item <?php if(!$rows->count()): ?> not-found <?php endif; ?>">
+    <?php if($rows->count()): ?>
+        <div class="text-paginate">
+            <h2 class="text">
+                <?php if($rows->total() > 1): ?>
+                    <?php echo e(__(":count activities found",['count'=>$rows->total()])); ?>
+                <?php else: ?>
+                    <?php echo e(__(":count activity found",['count'=>$rows->total()])); ?>
+                <?php endif; ?>
+            </h2>
+            <span class="count-string"><?php echo e(__("Showing :from - :to of :total Activities",["from"=>$rows->firstItem(),"to"=>$rows->lastItem(),"total"=>$rows->total()])); ?></span>
+        </div>
+        <div class="list-item">
+            <div class="row">
+                <?php $__currentLoopData = $rows; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <div class="col-lg-4 col-md-6">
+                        <?php echo $__env->make('Activity::frontend.layouts.search.loop-gird', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                    </div>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </div>
+        </div>
+
+        <div class="bravo-pagination">
+            <?php echo e($rows->appends(array_merge(request()->query(),['_ajax'=>1]))->links()); ?>
+        </div>
+    <?php else: ?>
+        <div class="not-found-box">
+            <h3 class="n-title"><?php echo e(__("We couldn't find any activities.")); ?></h3>
+            <p class="p-desc"><?php echo e(__("Try changing your filter criteria")); ?></p>
+            
+        </div>
+    <?php endif; ?>
+</div>
+<?php /**PATH D:\Web\Laravel\VarghaJob\bookme\modules/Activity/Views/frontend/layouts/search-map/list-item.blade.php ENDPATH**/ ?>
