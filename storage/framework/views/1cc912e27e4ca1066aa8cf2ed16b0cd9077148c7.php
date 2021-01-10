efa30c64c438e7c288e7c3f7f28ecc48e9369e99
LunA721 <rh7214you@gmail.com>
2021-01-11 01:15:17 +0900

    change form search style

--- storage/framework/views/1cc912e27e4ca1066aa8cf2ed16b0cd9077148c7.php
+++ storage/framework/views/1cc912e27e4ca1066aa8cf2ed16b0cd9077148c7.php
@@ -1,4 +1,7 @@
-9999999999s:19589:"a,
+<?php $main_color = setting_item('style_main_color','#5191fa');
+$style_typo = json_decode(setting_item_with_lang('style_typo',false,"{}"),true);
+?>
+    a,
     .bravo-news .btn-readmore,
     .bravo_wrap .bravo_header .content .header-left .bravo-menu ul li:hover > a,
     .bravo_wrap .bravo_search_tour .bravo_form_search .bravo_form .field-icon,
@@ -13,11 +16,11 @@
     .hotel_rooms_form .form-search-rooms .form-search-row>div .form-group .render,
     .bravo_wrap .bravo_form .form-content .render,
     a:hover {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo-pagination ul li.active a, .bravo-pagination ul li.active span
     {
-        color:#942752!important;
+        color:<?php echo e($main_color); ?>!important;
     }
     .bravo-news .widget_category ul li span,
     .bravo_wrap .bravo_search_tour .bravo_form_search .bravo_form .g-button-submit button,
@@ -36,25 +39,25 @@
     .bravo_wrap .bravo_detail_space .bravo-list-hotel-related-widget .heading:after,
     .btn-primary:not(:disabled):not(.disabled):active
     {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
 
     .bravo-pagination ul li.active a, .bravo-pagination ul li.active span
     {
-        border-color:#942752!important;
+        border-color:<?php echo e($main_color); ?>!important;
     }
     .bravo_wrap .bravo_search_tour .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-from:before, .bravo_wrap .bravo_search_tour .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-to:before, .bravo_wrap .bravo_search_tour .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single:before,
     .bravo-reviews .review-form .form-wrapper,
     .bravo_wrap .bravo_detail_tour .bravo_content .bravo_tour_book
     {
-        border-top-color:#942752;
+        border-top-color:<?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_footer .main-footer .nav-footer .context .contact{
-        border-left-color:#942752;
+        border-left-color:<?php echo e($main_color); ?>;
     }
     .hotel_rooms_form .form-search-rooms{
-        border-bottom-color:#942752;
+        border-bottom-color:<?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_form .field-icon,
@@ -68,35 +71,35 @@
     .bravo-reviews .review-box .review-box-score .review-score-base span,
     .bravo_wrap .bravo_detail_tour .bravo_content .g-header .review-score .head .left .text-rating
     {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_form .smart-search .parent_text::-webkit-input-placeholder{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_form .smart-search .parent_text::-moz-placeholder{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_form .smart-search .parent_text::-ms-input-placeholder{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_form .smart-search .parent_text::-moz-placeholder{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_form .smart-search .parent_text::placeholder{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
 
     .bravo_wrap .bravo_search_space .bravo-list-item .list-item .item-loop .service-review .rate,
     .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .btn-apply-price-range{
 
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .page-template-content .bravo-list-locations.style_2 .list-item .destination-item:hover .title,
     .bravo_wrap .page-template-content .bravo-list-space .item-loop .sale_info,
@@ -113,89 +116,89 @@
     .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single,
     .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-handle>i:first-child
     {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-from:before, .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-to:before, .bravo_wrap .bravo_search_space .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single:before {
-        border-top-color: #942752;
+        border-top-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_space .bravo_content .g-overview ul li:before {
-        border: 1px solid #942752;
+        border: 1px solid <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_space .bravo-list-space-related .item-loop .sale_info {
-        background-color: #942752;
+        background-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_space .bravo_content .g-header .review-score .head .score::after {
-        border-bottom: 25px solid #942752;
+        border-bottom: 25px solid <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_space .bravo_content .bravo_space_book {
-        border-top: 5px solid #942752;
+        border-top: 5px solid <?php echo e($main_color); ?>;
     }
 
     body .daterangepicker.loading:after {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     body .daterangepicker .drp-calendar .calendar-table tbody tr td.end-date {
-        border-right: solid 2px #942752;
+        border-right: solid 2px <?php echo e($main_color); ?>;
     }
     body .daterangepicker .drp-calendar .calendar-table tbody tr td.start-date {
-        border-left: solid 2px #942752;
+        border-left: solid 2px <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_space .bravo-list-space-related .item-loop .service-review .rate {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .has-search-map .bravo-filter-price .irs--flat .irs-bar,
     .has-search-map .bravo-filter-price .irs--flat .irs-handle>i:first-child,
     .has-search-map .bravo-filter-price .irs--flat .irs-from, .has-search-map .bravo-filter-price .irs--flat .irs-to, .has-search-map .bravo-filter-price .irs--flat .irs-single {
-        background-color: #942752;
+        background-color: <?php echo e($main_color); ?>;
     }
 
     .has-search-map .bravo-filter-price .irs--flat .irs-from:before, .has-search-map .bravo-filter-price .irs--flat .irs-to:before, .has-search-map .bravo-filter-price .irs--flat .irs-single:before {
-        border-top-color: #942752;
+        border-top-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_tour .bravo_content .g-header .review-score .head .score {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_tour .bravo_content .g-header .review-score .head .score::after {
-        border-bottom: 25px solid #942752;
+        border-bottom: 25px solid <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_tour .bravo_content .g-overview ul li:before {
-        border: 1px solid #942752;
+        border: 1px solid <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_location .bravo_content .g-location-module .location-module-nav li a.active {
-        border-bottom: 1px solid #942752;
-        color: #942752;
+        border-bottom: 1px solid <?php echo e($main_color); ?>;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_location .bravo_content .g-location-module .item-loop .sale_info {
-        background-color: #942752;
+        background-color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .page-template-content .bravo-featured-item.style2 .number-circle {
-        border: 2px solid #942752;
-        color: #942752;
+        border: 2px solid <?php echo e($main_color); ?>;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .page-template-content .bravo-featured-item.style3 .featured-item:hover {
-        border-color: #942752;
+        border-color: <?php echo e($main_color); ?>;
     }
 
     .booking-success-notice .booking-info-detail {
-        border-left: 3px solid #942752;
+        border-left: 3px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_tour .bravo_single_book,
     .bravo_wrap .bravo_detail_space .bravo_single_book {
-        border-top: 5px solid#942752;
+        border-top: 5px solid<?php echo e($main_color); ?>;
     }
     .bravo_wrap .page-template-content .bravo-form-search-all .g-form-control .nav-tabs li a.active {
-        background-color: #942752;
-        border-color: #942752;
+        background-color: <?php echo e($main_color); ?>;
+        border-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_location .bravo_content .g-location-module .item-loop .service-review .rate,
@@ -207,14 +210,14 @@
     .bravo_wrap .page-template-content .bravo-list-tour.box_shadow .list-item .item .caption .title-address .title a:hover,
     .bravo_wrap .bravo_search_hotel .bravo-list-item .list-item .item-loop .service-review .rate,
     .bravo_wrap .bravo_search_hotel .bravo_filter .g-filter-item .item-content .btn-apply-price-range {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_tour .bravo-list-tour-related .item-tour .featured ,
     .bravo_wrap .bravo_search_tour .bravo-list-item .list-item .item-tour .featured,
     .bravo_wrap .page-template-content .bravo-list-tour .item-tour .featured,
     .bravo_wrap .bravo_search_hotel .bravo_filter .filter-title:before {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .page-template-content .bravo-list-tour.box_shadow .list-item .item .header-thumb .tour-book-now,
     .bravo_wrap .bravo_search_hotel .bravo-list-item .list-item .item-loop .sale_info,
@@ -232,16 +235,16 @@
     .bravo_wrap .bravo_search_hotel .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-from:before,
     .bravo_wrap .bravo_search_hotel .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-to:before,
     .bravo_wrap .bravo_search_hotel .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single:before {
-        border-top-color: #942752;
+        border-top-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_hotel .bravo-list-item .list-item .item-loop-list .service-review-pc .head .score,
     .bravo_wrap .bravo_search_hotel .bravo_content .g-header .review-score .head .score {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_hotel .bravo_content .g-overview ul li:before {
-        border: 1px solid #942752;
+        border: 1px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_search_hotel .bravo_filter .g-filter-item .item-content .btn-more-item,
     .bravo_wrap .bravo_search_hotel .bravo_content .g-header .review-score .head .left .text-rating,
@@ -249,48 +252,48 @@
     .bravo_wrap .bravo_detail_hotel  .btn-show-all,
     .bravo_wrap .bravo_detail_hotel  .bravo-list-hotel-related .item-loop .service-review .rate,
     .bravo_wrap .bravo_form .select-guests-dropdown .dropdown-item-row .count-display{
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_hotel .bravo-list-item .list-item .item-loop-list .service-review-pc .head .score::after {
-        border-bottom: 15px solid #942752;
+        border-bottom: 15px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_hotel .bravo_content .g-header .review-score .head .score:after {
-        border-bottom: 25px solid #942752;
+        border-bottom: 25px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_hotel .bravo_content .g-header .review-score .head .score {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_hotel .bravo-list-hotel-related-widget .heading:after {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_hotel .bravo_content .g-attributes h3:after {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_hotel .bravo_content .g-header .review-score .head .left .text-rating {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .select-guests-dropdown .dropdown-item-row .count-display {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo-checkbox input[type=checkbox]:checked+.checkmark:after {
-        border: solid #942752;
+        border: solid <?php echo e($main_color); ?>;
         border-width: 0 2px 2px 0;
     }
     .bravo_wrap .bravo_form .input-search .form-control::-webkit-input-placeholder {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_form .input-search .form-control:-ms-input-placeholder {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .brav_wrap .bravo_form .input-search .form-control::placeholder {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .btn-apply-price-range{
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_search_event .bravo_filter .filter-title:before,
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-bar,
@@ -302,75 +305,67 @@
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single,
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-handle>i:first-child
     {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-from:before,
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-to:before,
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .bravo-filter-price .irs--flat .irs-single:before {
-        border-top-color: #942752;
+        border-top-color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_search_event .bravo_filter .g-filter-item .item-content .btn-more-item {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_event .bravo_content .g-header .review-score .head .score:after {
-        border-bottom: 25px solid #942752;
+        border-bottom: 25px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_event .bravo_content .g-header .review-score .head .score {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_event .bravo_content .g-header .review-score .head .left .text-rating {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_single_book .nav-enquiry .enquiry-item.active span {
-        border-bottom: solid 1px #942752 !important;
-        color: #942752 !important;
+        border-bottom: solid 1px <?php echo e($main_color); ?> !important;
+        color: <?php echo e($main_color); ?> !important;
     }
     .bravo_wrap .bravo_detail_event .bravo_content .g-overview ul li:before {
-        border: 1px solid #942752;
+        border: 1px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_event .bravo_content .g-attributes .list-attributes .item i.icon-default {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_event .bravo_single_book {
-        border-top: 5px solid #942752;
+        border-top: 5px solid <?php echo e($main_color); ?>;
     }
 
     .bravo_wrap .bravo_detail_hotel .bravo_single_book {
-        border-top: 5px solid #942752;
+        border-top: 5px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_car  .bravo_single_book {
-        border-top: 5px solid #942752;
+        border-top: 5px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_car .bravo_content .g-header .review-score .head .score:after {
-        border-bottom: 25px solid #942752;
+        border-bottom: 25px solid <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_car .bravo_content .g-header .review-score .head .score {
-        background: #942752;
+        background: <?php echo e($main_color); ?>;
     }
     .bravo_wrap .bravo_detail_car .bravo_content .g-header .review-score .head .left .text-rating {
-        color: #942752;
+        color: <?php echo e($main_color); ?>;
     }
 
     body{
-    
-        
-            
-                font-family:Poppins;
-            
-        
-            
-        
-            
-        
-            
-        
-            
-        
-    
+    <?php if(!empty($style_typo) && is_array($style_typo)): ?>
+        <?php $__currentLoopData = $style_typo; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $k=>$v): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <?php if($v): ?>
+                <?php echo e(str_replace('_','-',$k)); ?>:<?php echo $v; ?>;
+            <?php endif; ?>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
     }
 
-    
-    ";
+    <?php echo (setting_item('style_custom_css')); ?>
+    <?php echo (setting_item_with_lang_raw('style_custom_css')); ?><?php /**PATH D:\Web\Laravel\VarghaJob\bookme\modules/Layout/parts/custom-css.blade.php ENDPATH**/ ?>
