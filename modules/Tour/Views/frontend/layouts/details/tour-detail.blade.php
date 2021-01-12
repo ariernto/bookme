10f9a15143db5645203f08ddaf4405da971ed6b2
LunA721 <rh7214you@gmail.com>
2021-01-12 23:47:51 +0800

    finish 3 pages

--- modules/Tour/Views/frontend/layouts/details/tour-detail.blade.php
+++ modules/Tour/Views/frontend/layouts/details/tour-detail.blade.php
@@ -1,160 +1,139 @@
-<div class="g-header">
-    <div class="left">
-        <h1>{{$translation->title}}</h1>
-        @if($translation->address)
-            <p class="address"><i class="fa fa-map-marker"></i>
-                {{$translation->address}}
-            </p>
-        @endif
-    </div>
-    <div class="right">
-        @if(setting_item('tour_enable_review') and $review_score)
-            <div class="review-score">
-                <div class="head">
-                    <div class="left">
-                        <span class="head-rating">{{$review_score['score_text']}}</span>
-                        <span class="text-rating">{{__("from :number reviews",['number'=>$review_score['total_review']])}}</span>
-                    </div>
-                    <div class="score">
-                        {{$review_score['score_total']}}<span>/5</span>
-                    </div>
-                </div>
-                <div class="foot">
-                    {{__(":number% of guests recommend",['number'=>$row->recommend_percent])}}
-                </div>
-            </div>
-        @endif
-    </div>
-</div>
-@if(!empty($row->duration) or !empty($row->category_tour->name) or !empty($row->max_people) or !empty($row->location->name))
-    <div class="g-tour-feature">
-    <div class="row">
-        @if($row->duration)
-            <div class="col-xs-6 col-lg-3 col-md-6">
-                <div class="item">
-                    <div class="icon">
-                        <i class="icofont-wall-clock"></i>
-                    </div>
-                    <div class="info">
-                        <h4 class="name">{{__("Duration")}}</h4>
-                        <p class="value">
-                            {{duration_format($row->duration,true)}}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        @endif
-        @if(!empty($row->category_tour->name))
-            @php $cat =  $row->category_tour->translateOrOrigin(app()->getLocale()) @endphp
-            <div class="col-xs-6 col-lg-3 col-md-6">
-                <div class="item">
-                    <div class="icon">
-                        <i class="icofont-beach"></i>
-                    </div>
-                    <div class="info">
-                        <h4 class="name">{{__("Tour Type")}}</h4>
-                        <p class="value">
-                            {{$cat->name ?? ''}}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        @endif
-        @if($row->max_people)
-            <div class="col-xs-6 col-lg-3 col-md-6">
-                <div class="item">
-                    <div class="icon">
-                        <i class="icofont-travelling"></i>
-                    </div>
-                    <div class="info">
-                        <h4 class="name">{{__("Group Size")}}</h4>
-                        <p class="value">
-                            @if($row->max_people > 1)
-                                {{ __(":number persons",array('number'=>$row->max_people)) }}
-                            @else
-                                {{ __(":number person",array('number'=>$row->max_people)) }}
-                            @endif
-                        </p>
-                    </div>
-                </div>
-            </div>
-        @endif
-        @if(!empty($row->location->name))
-                @php $location =  $row->location->translateOrOrigin(app()->getLocale()) @endphp
-            <div class="col-xs-6 col-lg-3 col-md-6">
-                <div class="item">
-                    <div class="icon">
-                        <i class="icofont-island-alt"></i>
-                    </div>
-                    <div class="info">
-                        <h4 class="name">{{__("Location")}}</h4>
-                        <p class="value">
-                            {{$location->name ?? ''}}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        @endif
-    </div>
-</div>
-@endif
-@if($row->getGallery())
-    <div class="g-gallery">
-        <div class="fotorama" data-width="100%" data-thumbwidth="135" data-thumbheight="135" data-thumbmargin="15" data-nav="thumbs" data-allowfullscreen="true">
-            @foreach($row->getGallery() as $key=>$item)
-                <a href="{{$item['large']}}" data-thumb="{{$item['thumb']}}" data-alt="{{ __("Gallery") }}"></a>
-            @endforeach
-        </div>
-        <div class="social">
-            <div class="social-share">
-                <span class="social-icon">
-                    <i class="icofont-share"></i>
-                </span>
-                <ul class="share-wrapper">
-                    <li>
-                        <a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u={{$row->getDetailUrl()}}&amp;title={{$translation->title}}" target="_blank" rel="noopener" original-title="{{__("Facebook")}}">
-                            <i class="fa fa-facebook fa-lg"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="twitter" href="https://twitter.com/share?url={{$row->getDetailUrl()}}&amp;title={{$translation->title}}" target="_blank" rel="noopener" original-title="{{__("Twitter")}}">
-                            <i class="fa fa-twitter fa-lg"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="service-wishlist {{$row->isWishList()}}" data-id="{{$row->id}}" data-type="{{$row->type}}">
-                <i class="fa fa-heart-o"></i>
-            </div>
-        </div>
-    </div>
-@endif
-@if($translation->content)
-    <div class="g-overview">
-        <h3>{{__("Overview")}}</h3>
-        <div class="description">
-            <?php echo $translation->content ?>
-        </div>
-    </div>
-@endif
-@include('Tour::frontend.layouts.details.tour-include-exclude')
-@include('Tour::frontend.layouts.details.tour-itinerary')
-@include('Tour::frontend.layouts.details.tour-attributes')
-@include('Tour::frontend.layouts.details.tour-faqs')
-@if($row->map_lat && $row->map_lng)
-<div class="g-location">
-    <div class="location-title">
-        <h3>{{__("Tour Location")}}</h3>
-        @if($translation->address)
-            <div class="address">
-                <i class="icofont-location-arrow"></i>
-                {{$translation->address}}
-            </div>
-        @endif
-    </div>
-
-    <div class="location-map">
-        <div id="map_content"></div>
-    </div>
-</div>
-@endif
+<div class="g-header">
+    <div class="left">
+        <h1>{{$translation->title}}</h1>
+        <div class="onerow">
+            @if($translation->address)
+                <p class="address"><i class="fa fa-map-marker"></i>
+                    {{$translation->address}}
+                </p>
+            @endif
+            <div class="starreview">
+                <i class="fa fa-star starcol"></i>
+                <i class="fa fa-star starcol"></i>
+                <i class="fa fa-star starcol"></i>
+                <i class="fa fa-star starcol"></i>
+                <i class="fa fa-star starcol"></i>
+                <span class="review">
+                    15 Reviews
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+@if(!empty($row->duration) or !empty($row->category_tour->name) or !empty($row->max_people) or !empty($row->location->name))
+    <div class="g-tour-feature">
+    <div class="row">
+        @if($row->duration)
+            <div class="col-xs-6 col-lg-3 col-md-6">
+                <div class="item">
+                    <div class="icon">
+                        <i class="icofont-wall-clock"></i>
+                    </div>
+                    <div class="info">
+                        <p class="value">
+                            {{duration_format($row->duration,true)}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endif
+        @if($row->max_people)
+            <div class="col-xs-6 col-lg-3 col-md-6">
+                <div class="item">
+                    <div class="icon">
+                        <i class="icofont-travelling"></i>
+                    </div>
+                    <div class="info">
+                        <p class="value">
+                            @if($row->max_people > 1)
+                                {{ __(":number persons",array('number'=>$row->max_people)) }}
+                            @else
+                                {{ __(":number person",array('number'=>$row->max_people)) }}
+                            @endif
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endif
+        @if(!empty($row->category_tour->name))
+            @php $cat =  $row->category_tour->translateOrOrigin(app()->getLocale()) @endphp
+            <div class="col-xs-6 col-lg-3 col-md-6">
+                <div class="item">
+                    <div class="icon">
+                        <i class="icofont-beach"></i>
+                    </div>
+                    <div class="info">
+                        <p class="value">
+                            {{$cat->name ?? ''}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endif
+        @if(!empty($row->location->name))
+                @php $location =  $row->location->translateOrOrigin(app()->getLocale()) @endphp
+            <div class="col-xs-6 col-lg-3 col-md-6">
+                <div class="item">
+                    <div class="icon">
+                        <i class="icofont-island-alt"></i>
+                    </div>
+                    <div class="info">
+                        <p class="value">
+                            {{$location->name ?? ''}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
+@endif
+@if($row->getGallery())
+    <div class="g-gallery">
+        <div class="fotorama" data-width="100%" data-thumbwidth="135" data-thumbheight="135" data-thumbmargin="15" data-nav="thumbs" data-allowfullscreen="true">
+            @foreach($row->getGallery() as $key=>$item)
+                <a href="{{$item['large']}}" data-thumb="{{$item['thumb']}}" data-alt="{{ __("Gallery") }}"></a>
+            @endforeach
+        </div>
+    </div>
+@endif
+<div class="g-overview">
+    <h3>{{__("Overview")}}</h3>
+    <div class="description">
+        The colorful flamingos welcome you as soon as you enter the grounds of the Honolulu Zoo. Nearby, over 160 other species of tropical birds also show off their bright plumage. Continue on for close encounters with impressive giant reptiles such as Komodo dragons, crocodiles and iguanas, before visiting the giraffes, meerkats, cheetahs and aardvarks
+       <br/><br/>
+       The 42-acre (16-hectare) grounds of the Honolulu Zoo are divided into three main areas: the Tropical Forests, the Pacific Islands and the African Savannah. Within these areas you will see over 250 different mammal, reptile and bird species in areas designed to closely replicate their natural habitats.
+    </div>
+</div>
+<div class="g-overview">
+    <h4>{{__("Additional information:")}}</h4>
+    <div class="description">
+        This tour has an age range of 12-70 years old, this means children under the age of 12 will not be eligible to participate in this tour. However, if you are over 70 years please contact us as you may be eligible to join the tour if you fill out G Adventures self-assessment form.
+    </div>
+</div>
+<div class="g-overview">
+    <h4>{{__("Dress Code:")}}</h4>
+    <div class="description">
+        Casual. Comfortable athletic clothing. hiking shoes, hat and warm jacket.
+    </div>
+</div>
+
+@include('Tour::frontend.layouts.details.tour-faqs')
+
+@if($row->map_lat && $row->map_lng)
+<div class="g-location">
+    <div class="location-title">
+        <h3>{{__("Tour Location")}}</h3>
+        @if($translation->address)
+            <div class="address">
+                <i class="icofont-location-arrow"></i>
+                {{$translation->address}}
+            </div>
+        @endif
+    </div>
+    <div class="location-map">
+        <div id="map_content"></div>
+    </div>
+</div>
+@endif
+
