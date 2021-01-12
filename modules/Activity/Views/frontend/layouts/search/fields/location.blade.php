384865941892ad23a267e0a33d8a0d77903653a2
LunA721 <rh7214you@gmail.com>
2021-01-12 23:47:51 +0800

    finish 3 pages

--- modules/Activity/Views/frontend/layouts/search/fields/location.blade.php
+++ modules/Activity/Views/frontend/layouts/search/fields/location.blade.php
@@ -1,17 +1,1 @@
-<div class="form-group">
-
-    <i class="field-icon fa icofont-search"></i>
-
-    <div class="form-content">
-
-        <label>{{ $field['title'] ?? "" }}</label>
-
-        <div class="input-search">
-
-            <input type="text" name="service_name" class="form-control" placeholder="{{__("Search for...")}}" value="{{ request()->input("service_name") }}">
-
-        </div>
-
-    </div>
-
-</div>
+<input type="text" name="service_name" class="form-control searchinput" placeholder="{{__("Destination, city, address")}}" value="{{ request()->input("service_name") }}">
