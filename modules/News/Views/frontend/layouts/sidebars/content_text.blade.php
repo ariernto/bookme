9a55da07986ef92d53440b01bd2b6d994cde2382
LunA721 <rh7214you@gmail.com>
2021-12-28 22:52:20 +0900

    remove space news

--- modules/News/Views/frontend/layouts/sidebars/content_text.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="sidebar-widget">
-    <div class="sidebar-title">
-        <h2>{{ $item->title }}</h2>
-    </div>
-    <div class="textwidget">
-        {{ $item->content }}
-    </div>
-</div>
